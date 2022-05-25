fc8e01dc39cfa6749ba5c5965121c1eb2f233d39
Shubham Pawade <Shubham.Pawade@bentley.com>
2022-05-25 18:17:02 +0530

    removed comments

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -8,15 +8,12 @@ expenses.read_expenses("data/spending_data.csv")
 spending_categories=[]
 
 for e in expenses.list:
-    # if e.category not in spending_categories:
     spending_categories.append(e.category)
         
 
 spending_counter=collections.Counter(spending_categories)
 top5_spending_categories=spending_counter.most_common(5)
 
-#getting list of keys and values from list of dectionaries
-#print(type(spending_counter))
 categories,count=zip(*top5_spending_categories)
 
 fig,ax=plt.subplots()
