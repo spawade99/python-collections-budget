3a41b7a282379dcaaa392a778c701d7ab547f4ef
Shubham Pawade <Shubham.Pawade@bentley.com>
2022-05-25 18:33:54 +0530

    renaming variables

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -7,8 +7,8 @@ expenses.read_expenses("data/spending_data.csv")
 
 spending_categories=[]
 
-for e in expenses.list:
-    spending_categories.append(e.category)
+for expense in expenses.list:
+    spending_categories.append(expense.category)
         
 
 spending_counter=collections.Counter(spending_categories)
